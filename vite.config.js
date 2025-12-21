ec4b35cba6d12b59e9feaf22f29126b59b63668f
Cindy <cindy@example.com>
2025-12-21 12:18:07 +0800

    改回 dist，使用 gh-pages action 自動部署

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/product_fr_React/',
   build: {
-    outDir: 'docs'
+    outDir: 'dist'
   },
   plugins: [react()],
   server: {
