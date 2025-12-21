42468dbb0c28694a0f7e1267f106feb72b361668
Cindy <cindy@example.com>
2025-12-21 12:06:36 +0800

    修復 vite 基礎路徑和 index.html

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/product_fr_React/',
   plugins: [react()],
   server: {
     port: 3000,
